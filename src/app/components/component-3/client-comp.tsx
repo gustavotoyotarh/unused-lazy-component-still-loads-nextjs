0e2e26681d60971a63585659a9c1ef94cbf10b7c
Gustavo Toyota <gustavo.toyota@retailhub.com.br>
2024-09-26 11:32:53 -0300

    feat: add lorem ipsum to unused components

--- src/app/components/component-3/client-comp.tsx
+++ src/app/components/component-3/client-comp.tsx
@@ -7,6 +7,56 @@ export function ClientComponent() {
         Client component 3: Unused lazy component, shouldn&apos;t appear on the
         browser
       </div>
+
+      <div>
+        Lorem ipsum dolor sit amet, consectetur adipiscing elit. Donec eget
+        interdum dolor, sed tincidunt ante. Sed rhoncus egestas aliquet. Nullam
+        facilisis laoreet nunc eget dictum. Vestibulum iaculis tortor eu varius
+        scelerisque. Aliquam porttitor ex dui, vel feugiat quam condimentum nec.
+        Nulla tortor libero, viverra ac tristique eget, viverra sit amet leo. Ut
+        nec ultrices augue. Pellentesque habitant morbi tristique senectus et
+        netus et malesuada fames ac turpis egestas. Praesent et ex magna. Lorem
+        ipsum dolor sit amet, consectetur adipiscing elit. Fusce iaculis ipsum
+        vel mi facilisis cursus. Nam eu faucibus mi. Quisque et justo viverra,
+        dapibus ipsum at, ullamcorper sapien. Quisque luctus tellus cursus mi
+        sollicitudin malesuada a at lacus. Nam lacinia maximus sem eget posuere.
+        Ut lobortis felis et tempus feugiat. Praesent semper mollis est nec
+        auctor. Quisque tincidunt finibus ante vitae scelerisque. Quisque varius
+        urna magna, vel bibendum erat luctus non. Pellentesque porta dapibus
+        dictum. Integer leo turpis, accumsan vitae tellus in, gravida vehicula
+        risus. Integer eget libero eget purus aliquet maximus at sed dui.
+        Vivamus vitae est tellus. Interdum et malesuada fames ac ante ipsum
+        primis in faucibus. Praesent ullamcorper ipsum tortor, eget varius augue
+        malesuada ornare. Duis malesuada ante nec enim laoreet rhoncus. Donec
+        scelerisque, turpis et maximus vestibulum, sapien nulla dignissim augue,
+        convallis lobortis ipsum leo feugiat justo. Integer ac urna turpis. Duis
+        vestibulum blandit augue a condimentum. Nulla pellentesque ante magna,
+        quis lobortis nunc consectetur in. Aenean congue urna quam. Vivamus
+        tristique, urna ut eleifend lacinia, turpis nisi blandit lectus, sit
+        amet ullamcorper libero augue faucibus tortor. Donec semper, massa
+        viverra vehicula fermentum, lectus tortor vulputate sapien, et
+        consectetur mi velit ullamcorper ante. Curabitur mattis tristique
+        tortor, eget rhoncus neque venenatis ut. Donec massa libero, pulvinar
+        eget massa a, congue congue odio. Etiam ullamcorper nibh eu ipsum
+        venenatis euismod. In sagittis tempus felis a pulvinar. Aliquam sapien
+        nisi, cursus nec nibh nec, aliquet fermentum neque. Donec at ligula eget
+        ligula consequat sodales. Mauris suscipit vulputate sapien, in eleifend
+        quam vestibulum viverra. Morbi auctor arcu eu ligula sagittis, vitae
+        dignissim lacus ultrices. Proin efficitur justo sit amet elementum
+        feugiat. Curabitur quis auctor leo, eget sodales justo. Nulla purus
+        elit, pretium sit amet eros ut, molestie imperdiet nibh. Phasellus id
+        ante ut nulla venenatis hendrerit in id tellus. Curabitur pretium
+        rhoncus pharetra. Aliquam ac nulla quis dui tempor porta vel ac mi.
+        Mauris ac pretium arcu. Nunc orci enim, molestie et augue a, ornare
+        tincidunt massa. Suspendisse vel lorem dui. Aliquam sed turpis auctor,
+        vulputate eros ac, luctus metus. Integer dapibus, nisi sit amet congue
+        euismod, mi diam facilisis risus, bibendum venenatis velit diam eu
+        libero. Phasellus ornare mattis semper. Integer convallis sem posuere
+        augue pulvinar finibus. Pellentesque habitant morbi tristique senectus
+        et netus et malesuada fames ac turpis egestas. Integer rutrum ultrices
+        sagittis. Curabitur eget neque eros. Donec vitae magna consectetur,
+        tincidunt ipsum ac, interdum purus.
+      </div>
     </>
   );
 }
